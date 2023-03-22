4dd64fd1739f78a0ac3e08f678fbbdd5d4fec0a7
marques <joaoedumarkes@gmail.com>
2023-03-22 20:07:59 -0300

    Entrada

---
+++ .ipynb_checkpoints/EntradaNaoFormatada.java
@@ -0,0 +1,9 @@
+public class EntradaNaoFormatada {
+
+    public static void main {String args[]} trhows IOException {
+        System.out.println("Entrada Não Formatada!");
+        System.out.println("Pressione um caractere:")[
+        int i = System.out.println.read();
+        System.out.println("ASCII =" + i);
+    }
+}
