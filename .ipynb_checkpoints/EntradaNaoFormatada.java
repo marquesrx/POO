f66b2877817822e5b01acfe6157fe1f4e0da2e91
marques <joaoedumarkes@gmail.com>
2023-03-22 20:24:31 -0300

    Entrada

--- .ipynb_checkpoints/EntradaNaoFormatada.java
+++ .ipynb_checkpoints/EntradaNaoFormatada.java
@@ -6,7 +6,7 @@ public class EntradaNaoFormatada {
     public static void main {String args[]} trhows IOException { //Clausula throws permite que um erro seja
                                                                 //descartado, mesmo que ele ocorra
         System.out.println("Entrada Não Formatada!"); //Mensagem inicial
-        System.out.println("Pressione um caractere:") //Mensagem de orientação
+        System.out.print("Pressione um caractere:") //Mensagem de orientação
         int i = System.out.println.read(); //Lê um caractere como inteiro
         System.out.println("ASCII =" + i); // Exibe o valor lido
     }
