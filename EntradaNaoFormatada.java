f3b57a760bbd1f233196b78f610e97c0eae584d1
marques <joaoedumarkes@gmail.com>
2023-03-23 08:57:00 -0300

    Teste

--- EntradaNaoFormatada.java
+++ EntradaNaoFormatada.java
@@ -3,11 +3,11 @@ import java.io.IOException; //Importação da classe IOExcpetion para tratar exc
 
 public class EntradaNaoFormatada {
 
-    public static void main {String args[]} trhows IOException { //Clausula throws permite que um erro seja
+    public static void main (String args[]) throws IOException { //Clausula throws permite que um erro seja
                                                                 //descartado, mesmo que ele ocorra
         System.out.println("Entrada Não Formatada!"); //Mensagem inicial
         System.out.print("Pressione um caractere:"); //Mensagem de orientação
-        int i = System.out.println.read(); //Lê um caractere como inteiro
+        int i = System.in.read(); //Lê um caractere como inteiro
         System.out.println("ASCII =" + i); // Exibe o valor lido
     }
 }
