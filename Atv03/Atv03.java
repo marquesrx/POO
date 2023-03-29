ceaf54854b5215e35cf62c20bc44153f67533efc
marques <joaoedumarkes@gmail.com>
2023-03-29 19:41:00 -0300

    Atv03

--- Atv03/Atv03.java
+++ Atv03/Atv03.java
@@ -1,11 +1,19 @@
+package Atv03;
+
 import javax.swing.JOptionPane;
+
 
 public class Atv03 {
     public static void main(String[] args) {
+        
+        //declaração de variaveis
+        String nota1, nota2, notaTrabalho;
+        double media;
+
         // solicita as notas das duas provas e do trabalho
-        String nota1 = JOptionPane.showInputDialog("Digite a nota da primeira prova:");
-        String nota2 = JOptionPane.showInputDialog("Digite a nota da segunda prova:");
-        String notaTrabalho = JOptionPane.showInputDialog("Digite a nota do trabalho:");
+        nota1 = JOptionPane.showInputDialog("Digite a nota da primeira prova:");
+        nota2 = JOptionPane.showInputDialog("Digite a nota da segunda prova:");
+        notaTrabalho = JOptionPane.showInputDialog("Digite a nota do trabalho:");
 
         // converte as notas de string para double
         double nota1Double = Double.parseDouble(nota1);
@@ -13,7 +21,7 @@ public class Atv03 {
         double notaTrabalhoDouble = Double.parseDouble(notaTrabalho);
 
         // calcula a média
-        double media = (nota1Double + nota2Double + notaTrabalhoDouble) / 3;
+        media = (nota1Double + nota2Double + notaTrabalhoDouble) / 3;
 
         // exibe a média na tela
         JOptionPane.showMessageDialog(null, "A média é: " + media);
--- EntradaNaoFormatada.java
+++ EntradaNaoFormatada.java
@@ -1,13 +1,12 @@
 import java.io.IOException; //Importação da classe IOExcpetion para tratar exceções 
 
-
 public class EntradaNaoFormatada {
 
-    public static void main (String args[]) throws IOException { //Clausula throws permite que um erro seja
-                                                                //descartado, mesmo que ele ocorra
-        System.out.println("Entrada Não Formatada!"); //Mensagem inicial
-        System.out.print("Pressione um caractere: "); //Mensagem de orientação
-        int i = System.in.read(); //Lê um caractere como inteiro
+    public static void main(String args[]) throws IOException { // Clausula throws permite que um erro seja
+                                                                // descartado, mesmo que ele ocorra
+        System.out.println("Entrada Não Formatada!"); // Mensagem inicial
+        System.out.print("Pressione um caractere: "); // Mensagem de orientação
+        int i = System.in.read(); // Lê um caractere como inteiro
         System.out.println("ASCII = " + i); // Exibe o valor lido
     }
 }
