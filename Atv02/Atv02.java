b773ca4cbc3a240b1d9212d4520a17873242f72e
marques <joaoedumarkes@gmail.com>
2023-03-29 20:13:31 -0300

    Atv02

---
+++ Atv02/Atv02.java
@@ -0,0 +1,23 @@
+package Atv02;
+
+public class Atv02 {     
+
+        public static void main(String[] args) {
+
+            int  a, c, b;
+            
+            //exibir mensagem solicitando um inteiro
+            System.out.print("Informe um inteiro: ");
+    
+            //atribuir o produto de b e c para a
+            b = 5;
+            c = 10;
+            a = (b + c);
+
+            System.out.println(a);
+    
+            //Este programa realiza um cálculo de exemplo de folha de pagamento
+            System.out.print("Este programa realiza um cálculo de exemplo de folha de pagamento");
+        }
+}
+
---
+++ Atv04/Atv04.java
@@ -0,0 +1,19 @@
+package Atv04;
+
+public class Atv04 {
+
+    public static void main(String[] args) {
+        // declaração e inicialização da variável gastosJaneiro
+        int gastosJaneiro = 30000;
+
+        // declaração e inicialização das variáveis gastosFevereiro e gastosMarco
+        double gastosFevereiro = 33030.77;
+        double gastosMarco = 23899.01;
+
+        // declaração e inicialização da variável gastosTrimestre
+        double gastosTrimestre = gastosJaneiro + gastosFevereiro + gastosMarco;
+
+        //exibição do valor da variável gastosTrimestre
+        System.out.println("O gasto total no trimestre foi: R$ " + gastosTrimestre);
+    }
+}
