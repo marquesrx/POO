ade95806ab72861bc8b7ef5f497f4bd8c1ac7b5e
João Marques <51685504+marquesrx@users.noreply.github.com>
2023-09-15 21:41:42 -0300

    Create veiculo.java

---
+++ Atv04 3 - Bim/veiculo.java
@@ -0,0 +1,31 @@
+// Classe abstrata Veiculo
+public abstract class Veiculo {
+    private String marca;
+    private String modelo;
+
+    // Construtor
+    public Veiculo(String marca, String modelo) {
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+
+    // Método abstrato para ligar o motor
+    public abstract void ligarMotor();
+
+    // Getters e setters para marca e modelo
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+}
